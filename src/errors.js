d6b1c4381a34e961929ef90474bce7a0b3cbbef7
jungdaniel <9743834+jungdaniel@users.noreply.github.com>
2021-02-01 09:46:26 +0100

    feat(errors.js): PLAY-2165 define errors

---
+++ src/errors.js
@@ -0,0 +1,5 @@
+export const ERRORS = {
+	INVALID_IFRAME_ID: 'Reference found by id is null, it must a HTMLIFrameElement',
+	INVALID_IFRAME_REFERENCE: 'Reference is not an instance of HTMLIFrameElement',
+	INVALID_IFRAME_SRC: 'Invalid iframe source',
+}
