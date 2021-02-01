9ab04ca48d468fe8e232f6b9c04e96fcd395c4d8
jungdaniel <9743834+jungdaniel@users.noreply.github.com>
2021-02-01 09:49:36 +0100

    test(iframe.js): PLAY-2165 refactor iframe mock

---
+++ tests/mocks/iframe.js
@@ -0,0 +1,78 @@
+function getAttribute(attr) {
+	switch (attr) {
+		case 'src':
+			return 'http://localhost/frame.html';
+		default:
+			return null;
+	}
+}
+
+module.exports = ({ id, ready } = {}) => {
+	let messageHandlers = [];
+	let instance;
+
+	function sendEvent(eventData) {
+		const evt = document.createEvent('Event');
+		evt.initEvent('message', true, true);
+		evt.origin = 'http://localhost';
+		evt.source = instance.id;
+		evt.data = JSON.stringify(eventData);
+
+		window.dispatchEvent(evt);
+	}
+
+	function send(event, data, property) {
+		let eventData = {};
+		if (!property) {
+			eventData.event = {};
+			eventData.event[event] = data;
+		} else {
+			eventData.property = {};
+			eventData.property[property] = data;
+		}
+
+		sendEvent(eventData);
+	}
+
+	function socialsend(cmd, args) {
+		const eventData = { cmd, args, sstream: true };
+		sendEvent(eventData);
+	}
+
+	function postMessage(data, host) {
+		const { cmd } = JSON.parse(data);
+		switch (cmd) {
+			case 'probe':
+				if (ready) {
+					send('ready', true);
+				}
+				break;
+			case 'duration':
+			case 'viewers':
+			case 'progress':
+			case 'playingContent':
+				send(null, 100, cmd);
+				break;
+			// no default
+		}
+
+		messageHandlers.forEach(cb => cb.call(window, data, host));
+	}
+
+	function on(_, cb) {
+		messageHandlers.push(cb);
+	}
+
+	instance = {
+		id,
+		contentWindow: {
+			postMessage,
+		},
+		getAttribute,
+		on,
+		send,
+		socialsend,
+	};
+
+	return instance;
+};
